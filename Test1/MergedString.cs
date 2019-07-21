a0ba6d78310f6459f721af5bba2e8210caf94cf4
Giovanny <giovanny264@hotmail.com>
2019-07-21 18:00:43 -0500

    Upload

---
+++ Test1/MergedString.cs
@@ -0,0 +1,30 @@
+﻿namespace Test1
+{
+    public class MergedString
+    {
+        public string MergedText(string text1, string text2)
+        {
+            string mixed = string.Empty;
+
+            for(var i = 0; i < text1.Length; i++)
+            {
+                mixed += text1[i].ToString();
+                if(i < text2.Length)
+                {
+                    mixed += text2[i].ToString();
+                    if(i == text1.Length - 1)
+                    {
+                        i++;
+                        while(i < text2.Length)
+                        {
+                            mixed += text2[i].ToString();
+                            i++;
+                        }
+                    }
+                }
+            }
+
+            return mixed;
+        }
+    }
+}
---
+++ Test1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ask the user to type the first text.
+            Console.Write("Type a text, and then press Enter: ");
+            var input1 = Console.ReadLine();
+
+            // Ask the user to type the second text.
+            Console.Write("Type a text, and then press Enter: ");
+            var input2 = Console.ReadLine();
+
+            MergedString mergedString = new MergedString();
+            var result = mergedString.MergedText(input1, input2);
+
+            Console.Write("Result Merged String: " + result);
+            Console.ReadLine();
+        }
+    }
+}
